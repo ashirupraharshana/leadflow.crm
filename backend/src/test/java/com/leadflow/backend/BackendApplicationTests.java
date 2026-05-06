98bdd81ae01d779f57518ae8cd4e826899132abd
ashirupraharshana <ashirupraharshana@gmail.com>
2026-05-06 12:47:21 +0530

    chore: initialize LeadFlow CRM project

---
+++ backend/src/test/java/com/leadflow/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leadflow.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
